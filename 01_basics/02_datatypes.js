64ad6451bdf4f8bf99a39dfce3187be2ae86dc9c
Harish Sarki <53606218+sarkiharish@users.noreply.github.com>
2023-09-05 05:57:36 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict" //treat all js code as newer version
+
+//alert("Hi Harish") //we are using nodejs, not browser
+
+let name = "Harish"
+let age = 20
+let isLoggedIn = true
+let state;
+
+/*
+different types of datatypes in js:
+i. number
+ii. bigint
+iii. string => ""
+iv. boolean => true / false
+v. undefined
+vi. null => standalone value
+vii. symbol => unique
+
+viii. object
+
+*/
+
+console.log(typeof(undefined))  //undefined
+console.log(typeof(null)) //object
