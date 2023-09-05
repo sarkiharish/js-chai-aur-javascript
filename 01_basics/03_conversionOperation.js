2419d394a241efaaca15c3a5ea40f569693d1e37
Harish Sarki <53606218+sarkiharish@users.noreply.github.com>
2023-09-05 07:03:26 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = 77
+
+//console.log(typeof score)
+//console.log(typeof(score))
+
+let vlaueInNumber = Number(score)
+//console.log(typeof vlaueInNumber)
+
+//console.log(vlaueInNumber)
+
+//"77" => 77
+//"77mno" => NaN
+//true => 1; false => 0
+//null => 0
+//undefined => NaN
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+/*
+1 => true; 0 => false;
+"" => false
+"Harish" => true
+*/
+
+let someNumber = 18
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)
