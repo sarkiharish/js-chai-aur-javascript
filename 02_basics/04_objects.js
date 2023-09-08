1c2c6e1f5fe72e2409cdb3a61cfa4ed3e23426f4
Harish Sarki <53606218+sarkiharish@users.noreply.github.com>
2023-09-08 07:16:25 +0000

    json

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -17,17 +17,17 @@ const regularUser = {
     }
 }
 
-console.log(regularUser.fullName.userFullName.firstName)
+// console.log(regularUser.fullName.userFullName.firstName)
 
 const obj1 = {1:'a', 2: 'b'}
 const obj2 = {3:'a', 4: 'b'}
 const obj3 = {5:'a', 6: 'b'}
 
 const comboAssignObj = Object.assign({}, obj1, obj2, obj3)
-console.log(comboAssignObj)
+// console.log(comboAssignObj)
 
 const comboSpreadObj = {...obj1, ...obj2, ...obj3}
-console.log(comboSpreadObj)
+// console.log(comboSpreadObj)
 
 const users = [
     {
@@ -40,9 +40,19 @@ const users = [
     },
 ]
 
-console.log(users[1].name)
+// console.log(users[1].name)
 
-console.log(tinderUser)
-console.log(Object.keys(tinderUser))
-console.log(Object.values(tinderUser))
-console.log(Object.entries(tinderUser))
+// console.log(tinderUser)
+// console.log(Object.keys(tinderUser))
+// console.log(Object.values(tinderUser))
+// console.log(Object.entries(tinderUser))
+
+const course = {
+    courseName: "Javascript in Hindi",
+    courseFee: 999,
+    courseInstructor:"H Choudhary"
+}
+
+const {courseInstructor : instructor} = course;
+// console.log(courseInstructor)
+console.log(instructor);
