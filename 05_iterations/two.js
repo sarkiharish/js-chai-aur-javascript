d0fba5d66b4d54ad82d64eee2f8434c8c3f56eef
Harish Sarki <53606218+sarkiharish@users.noreply.github.com>
2023-09-13 05:49:52 +0000

    while and do while in js

---
+++ 05_iterations/two.js
@@ -0,0 +1,18 @@
+let index = 0
+while (index <= 10) {
+    console.log(`value of index is ${index}`)
+    index = index+2
+}
+
+const myArr = ["Ram", "Shyam", "Hari"]
+let i = 0
+while(i < myArr.length) {
+    console.log(myArr[i])
+    i++
+}
+
+let score = 11
+do {
+    console.log(`value is ${score}`)
+    score++
+} while (score <= 10);
